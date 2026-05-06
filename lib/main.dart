a308cc57ce4551558c9c3c67f22be1a099b44b80
Erik Schmidt <erik.sch2002@gmail.com>
2026-05-06 11:11:58 +0200

    added BottomNavigationBar + seperate Pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/meals.dart';
+import 'pages/pref.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,23 +16,65 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 7, 187, 181),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedElement = 0;
+
+  static final List<Widget> _pages = <Widget>[
+    Home(),
+    const MealsPage(),
+    const PreferencesPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedElement = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('test'),
+        title: const Text('Test Test'),
+        elevation: 12,
+        scrolledUnderElevation: 4,
+        centerTitle: true,
+      ),
+      body: Center(child: _pages.elementAt(_selectedElement)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outlined),
+            label: 'Add',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Preferences',
+          ),
+        ],
+        currentIndex: _selectedElement,
+        unselectedItemColor: Color.fromARGB(100, 10, 10, 10),
+        onTap: _onItemTapped,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
       ),
     );
   }
