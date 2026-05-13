f24c89e7297a5451e5dd319abe1303c85508191c
Erik Schmidt <erik.sch2002@gmail.com>
2026-05-13 14:32:31 +0200

    added functionatlity to add meal button, meals clickable

--- lib/pages/meals.dart
+++ lib/pages/meals.dart
@@ -8,19 +8,49 @@ class MealsPage extends StatefulWidget {
 }
 
 class _MealsPageState extends State<MealsPage> {
-
   List<Meals> mealList = [
     Meals(name: 'doener(chicken)', calorie: 179, fat: 8, protein: 9, carbs: 5),
     Meals(name: 'pizza salami', calorie: 270, fat: 13, protein: 9, carbs: 28),
-    Meals(name: 'schnitzel and fries', calorie: 220, fat: 45, protein: 45, carbs: 113),
-    Meals(name: 'oat flakes', calorie: 168, fat: 4.5, protein: 10.4, carbs: 20.9),
-    Meals(name: 'Pasta Aglio e Olio', calorie: 276, fat: 10, protein: 7.5, carbs: 39),
+    Meals(
+      name: 'schnitzel and fries',
+      calorie: 220,
+      fat: 45,
+      protein: 45,
+      carbs: 113,
+    ),
+    Meals(
+      name: 'oat flakes',
+      calorie: 168,
+      fat: 4.5,
+      protein: 10.4,
+      carbs: 20.9,
+    ),
+    Meals(
+      name: 'Pasta Aglio e Olio',
+      calorie: 276,
+      fat: 10,
+      protein: 7.5,
+      carbs: 39,
+    ),
   ];
+
+  //add meal to existing MealList
+  List<Meals> updateList(Meals meal) {
+    setState(() {
+      mealList.add(meal);
+    });
+    return mealList;
+  }
+
+  //dismiss Dialog popup
+  void _dismissDialog() {
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center( 
+      body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
@@ -29,47 +59,178 @@ class _MealsPageState extends State<MealsPage> {
               child: ListView.separated(
                 padding: const EdgeInsets.all(16),
                 itemCount: mealList.length,
-                itemBuilder:(context, int index) {
-                  return Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 68, 64, 84),
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 68, 64, 84),
-                        width: 3.0
+                itemBuilder: (context, int index) {
+                  return Card(
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Text(mealList[index].name),
+
+                            scrollable: true,
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text('${mealList[index].calorie}'),
+                                Text(
+                                  '${mealList[index].fat}',
+                                  textAlign: TextAlign.left,
+                                ),
+                                Text('${mealList[index].protein}'),
+                                Text('${mealList[index].carbs}'),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      child: SizedBox(
+                        width: 300,
+                        height: 90,
+                        child: Text(
+                          mealList[index].name,
+                          style: const TextStyle(
+                            fontSize: 26.0,
+                            color: Color.fromARGB(255, 68, 64, 84),
+                          ),
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(10.0),
-                      boxShadow: [BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        offset: Offset(4, 6),
-                        blurRadius: 10,
-                        spreadRadius: 2,
-                      )]
                     ),
-                    child: Text(mealList[index].name, style: TextStyle(fontSize: 26.0, color: const Color.fromARGB(255, 255, 255, 255))),
                   );
                 },
-                separatorBuilder: (context, index) => const SizedBox(height: 22.0),
-                ),
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 22.0),
+              ),
             ),
-            IconButton(icon: Icon(Icons.add_circle_outlined, size: 64,),
+            IconButton(
+              icon: Icon(Icons.add_circle_outlined, size: 64),
               onPressed: () {
-                
+                Meals tempMeal = Meals(
+                  name: '',
+                  calorie: 0,
+                  fat: 0,
+                  protein: 0,
+                  carbs: 0,
+                );
+
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Add Meal (100g)'),
+                    scrollable: true,
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Name',
+                          ),
+                          onChanged: (String value) {
+                            tempMeal.name = value;
+                            print(tempMeal.name);
+                          },
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Calorie',
+                          ),
+                          keyboardType: TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          onChanged: (String value) {
+                            int? parsedInput = int.tryParse(value);
+                            tempMeal.calorie = parsedInput;
+                            print(tempMeal.calorie);
+                          },
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Fat',
+                          ),
+                          keyboardType: TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          onChanged: (String value) {
+                            double? parsedInput = double.tryParse(value);
+                            tempMeal.fat = parsedInput;
+                            print(tempMeal.fat);
+                          },
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Protein',
+                          ),
+                          keyboardType: TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          onChanged: (String value) {
+                            double? parsedInput = double.tryParse(value);
+                            tempMeal.protein = parsedInput;
+                            print(tempMeal.protein);
+                          },
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Carbs',
+                          ),
+                          keyboardType: TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          onChanged: (String value) {
+                            double? parsedInput = double.tryParse(value);
+                            tempMeal.carbs = parsedInput;
+                            print(tempMeal.carbs);
+                          },
+                        ),
+                        Row(
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                _dismissDialog();
+                              },
+                              child: Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                if (tempMeal.name.isNotEmpty &&
+                                    tempMeal.calorie != 0) {
+                                  updateList(tempMeal);
+                                  _dismissDialog();
+                                } else {
+                                  return;
+                                }
+                              },
+                              child: Text('Save'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
               },
-              color: const Color.fromARGB(255, 47, 36, 58)) 
-          ]
-        )
-      )
+              color: const Color.fromARGB(255, 47, 36, 58),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class Meals {
   String name;
-  int calorie;
-  double fat;
-  double protein;
-  double carbs;
+  int? calorie;
+  double? fat;
+  double? protein;
+  double? carbs;
 
   Meals({
     required this.name,
@@ -79,4 +240,3 @@ class Meals {
     required this.carbs,
   });
 }
-
